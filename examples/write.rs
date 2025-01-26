411b4d0624d4ee62b750a9edfb6f8ad658c28909
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-01-25 23:17:41 -0600

    Remove comment

--- examples/write.rs
+++ examples/write.rs
@@ -6,8 +6,7 @@
 //         ("ARRAY1", vec![1, 2, 3].into()),
 //         ("ARRAY2", vec![4, 5, 7].into()),
 //     ];
-
-//     // Note: This will error if run outside of `cargo build`
+//
 //     write_byte_arrays("output.rs", byte_arrays, None).unwrap();
 
 //     // println!("{:?}", ARRAY2);
